5f1f88c902a0648ae2bca23e79c9cb071aef6878
Kavini Gayanthi <kavinigayanthi21@gmail.com>
2025-05-05 08:03:58 +0530

    add adnimitation file

--- src/home.jsx
+++ src/home.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import './home.css'
 import { IoIosCall } from "react-icons/io";
+import { motion} from 'framer-motion';
+import { FadeIn} from "./animation_variation";
+
 const Home =() =>{
     return (
         <>
         <div className='home'>
             <div className='container'>
             <div className='details'>
-                <div className='info'>
+                <motion.div 
+                variants= {FadeIn("up",0.2)}
+                initial ="hidden"
+                whileInView= {"show"}
+                viewport={{once: false, amount: 0.7}}
+                className='info'>
                     <h2>A creative design studio</h2>
-                    <p>We're a creative design studio speclalizing in meeting the needs of the new geeration. </p>
+                    <p>We're a creative design studio speclalizing in meeting the needs of<br></br> 
+                       the new geeration. We offer innovative and cutting-edge design<br></br>  
+                       solutios to help our clients stand out in today's fast-paced. </p>
 
 
-                </div>
+                </motion.div>
                 <div className='user'>
                     <div className='userimg_box'>
                         <img src='img/p.png' width={50}/>
@@ -33,7 +43,7 @@ const Home =() =>{
                    </div>
             </div>
             <div className='img_box'>
-                <img src="img/ppppp.jpg" />
+                <img src="img/p6.jpg" />
 
             </div>
 
