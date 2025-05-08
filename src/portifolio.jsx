b080036f9cadb2ca9c612597390ca9a8bce5be44
Kavini Gayanthi <kavinigayanthi21@gmail.com>
2025-05-08 07:19:26 +0530

    add image

--- src/portifolio.jsx
+++ src/portifolio.jsx
@@ -38,7 +38,59 @@ const portifolio = () => {
                       <p>DEveloping the look and feel of physical products, aesthetics and functionality.</p>
               </div>
               <div className='img_box'>
-                <img src='UIDesign.jpg' />
+                <img src='img/image5.jpg' width={50} height={50} />
+
+              </div>
+
+            </div>
+           </SwiperSlide>
+           <SwiperSlide>
+            <div className='slide_box'>
+              <div className='details'>
+                      <h3>product Design</h3>
+                      <p>DEveloping the look and feel of physical products, aesthetics and functionality.</p>
+              </div>
+              <div className='img_box'>
+                <img src='img/image4.jpg'width={50} height={50} />
+
+              </div>
+
+            </div>
+           </SwiperSlide>
+           <SwiperSlide>
+            <div className='slide_box'>
+              <div className='details'>
+                      <h3>product Design</h3>
+                      <p>DEveloping the look and feel of physical products, aesthetics and functionality.</p>
+              </div>
+              <div className='img_box'>
+                <img src='img/image3.jpg'width={50} height={50} />
+
+              </div>
+
+            </div>
+           </SwiperSlide>
+           <SwiperSlide>
+            <div className='slide_box'>
+              <div className='details'>
+                      <h3>product Design</h3>
+                      <p>DEveloping the look and feel of physical products, aesthetics and functionality.</p>
+              </div>
+              <div className='img_box'>
+                <img src='img/image2.jpg' width={50} height={50} />
+
+              </div>
+
+            </div>
+           </SwiperSlide>
+           <SwiperSlide>
+            <div className='slide_box'>
+              <div className='details'>
+                      <h3>product Design</h3>
+                      <p>DEveloping the look and feel of physical products, aesthetics and functionality.</p>
+              </div>
+              <div className='img_box'>
+                <img src='img/image1.jpg'width={50} height={50} />
 
               </div>
 
